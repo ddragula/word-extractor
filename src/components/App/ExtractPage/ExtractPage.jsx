a0661f560bce8800422fb1182ba388c0f374bec3
Dawid Draguła <dawid.dragula@gmail.com>
2021-09-12 02:01:25 +0200

    Extract page

--- src/components/App/ExtractPage/ExtractPage.jsx
+++ src/components/App/ExtractPage/ExtractPage.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, TextareaAutosize, Typography} from "@material-ui/core";
+import extractPageStyles from "./extractPageStyles";
+import PlayForWorkIcon from '@material-ui/icons/PlayForWork';
 
 function ExtractPage() {
+    const [ submitButtonDisabled, setSubmitButtonDisabled ] = useState(true);
+
+    const textAreaChangeHandler = e => {
+        if (e.target.value) {
+            if (submitButtonDisabled === true)
+            {
+                setSubmitButtonDisabled(false);
+            }
+        } else {
+            if (submitButtonDisabled === false)
+            {
+                setSubmitButtonDisabled(true);
+            }
+        }
+    }
+
+    const classes = extractPageStyles();
+
     return (
-        <div>Extract Page</div>
+        <>
+            <Typography component="h1" variant="h4" color="textPrimary" gutterBottom>
+                Copy the text here:
+            </Typography>
+            <TextareaAutosize className={classes.textarea} onChange={textAreaChangeHandler} minRows={6} maxRows={26} />
+            <div className={classes.buttonDiv}>
+                <Button variant="contained" color="primary" size="large" disabled={submitButtonDisabled}>
+                    Extract Words <PlayForWorkIcon />
+                </Button>
+            </div>
+        </>
     );
 }
 
---
+++ src/components/App/ExtractPage/extractPageStyles.js
@@ -0,0 +1,23 @@
+import {makeStyles} from "@material-ui/core";
+
+const extractPageStyles = makeStyles(theme => ({
+    textarea: {
+        width: '100%',
+        padding: '18.5px 14px',
+        borderRadius: 4,
+        border: '1px solid grey',
+        borderColor: theme.palette.text.secondary,
+        '&:hover': {
+            borderColor: theme.palette.text.primary
+        },
+        backgroundColor: "transparent",
+        font: 'inherit'
+    },
+    buttonDiv: {
+        display: 'flex',
+        flexDirection: 'row-reverse',
+        marginTop: theme.spacing(1)
+    }
+}));
+
+export default extractPageStyles;
